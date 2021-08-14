4be7d5395ec48310f3635c131fde23a6548dfb90
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-08-15 00:55:11 +0900

    init

---
+++ src/main/java/com/dobby/board_0815/Board0815Application.java
@@ -0,0 +1,13 @@
+package com.dobby.board_0815;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board0815Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board0815Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dobby/board_0815/Board0815ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobby.board_0815;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board0815ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
